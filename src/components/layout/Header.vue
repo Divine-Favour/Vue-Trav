949756226b8485b069481c88bc8041feb6e8cb5b
Divine-Favour <divine-favour.o@ng.lopworks.com>
2020-02-13 16:27:22 +0100

    frist commit

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,43 @@
+<template>
+    <header class="header">
+        <h1>Todolist</h1>
+        <div id="nav">
+            <router-link to="/" class="whitel" >Home</router-link> |
+            <router-link to="/about">About</router-link>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>  
+    #nav {
+      padding: 30px;
+      text-decoration: none;
+    }
+
+    #nav a {
+      font-weight: bold;
+      color: #ffffff;
+    }
+
+    #nav a.router-link-exact-active {
+      color: #42b983;
+    }
+
+    .header {
+        background: #333;
+        color: #fff;
+        text-align: center;
+        padding: 10px;
+    }
+
+    .header a {
+        color: #fff;
+        padding-right: 5px;
+    }
+</style>
